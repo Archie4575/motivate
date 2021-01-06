8330e20ef83bd7cda94a3c87f43841e6ef3d9cab
Archer Fabling <archie4575@gmail.com>
2021-01-06 16:50:24 +0800

    Refined file reading and extra quotes

--- motivate/motivate.py
+++ motivate/motivate.py
@@ -4,7 +4,24 @@ import json
 import os
 import random
 import argparse
+import jsonschema
 
+data_file_schema = {
+    "type": "object",
+    "properties": {
+        "data": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "quote": {"type": "string"},
+                    "author": {"type": "string"}
+                },
+                "required": ["quote", "author"]
+            }
+        }
+    }
+}
 
 def getlink(file):
     if os.path.islink(file):
@@ -24,64 +41,30 @@ def quote():
         data_dir = os.path.join('/opt', 'motivate', 'data')
 
     try:
-        num_of_json = len([f for f in os.listdir(data_dir)
-                           if os.path.isfile(os.path.join(data_dir, f))])
+        data_files = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))] # get file names as list
     except FileNotFoundError:
         print("Can't find the data folder. You probably haven't run 'install.sh' yet.")
         exit(1)
 
-    rand_no = random.randint(1, num_of_json)
-    filename = os.path.join(data_dir, str(rand_no).zfill(3) + '.json')
+
+    filename = os.path.join(data_dir, data_files[random.randrange(len(data_files))]) # pick a random file and construct a path
+    
     with open(filename) as json_data:
         try:
             quotes = json.load(json_data)
-        except ValueError:
-            print ("---------------Debug info begins:--------------")
-            print("Oops, we met a ValueError.")
-            print("Please check this file "+filename)
-            print("1. A Possible reason is that there is a redundant comma behind last group of author/quote in this json file.")
-            print("   If so, delete that redundant comma, then it will run smoothly.")
-            print("2. Another possible reason is that there is hard line-break or tab in that file.")
-            print("   However JSON don't support that. Please use '\\n' or '\\t'.")
-            print("For later actions, I help you wrote this filename to JSON_ERROR_LIST.txt.")
-            print("I suggest you to test those json file in this website: jsonlint.com")
-            f_tmp = open('JSON_ERROR_LIST.txt', "a")
-            f_tmp.write(filename+"\n")
-            f_tmp.close()
-            print ("---------------Debug info ends:--------------")
-            return
+            jsonschema.validate(instance=quotes, schema=data_file_schema)
+        except ValueError as value_err:
+            print("ValueError in file {0}\n{1}".format(filename, value_err))
+            exit(1)
+        except jsonschema.exceptions.ValidationError as validation_err:
+            print("ValidationError in file {0}\n{1}".format(filename, validation_err.message))
+            exit(1)
 
-        ran_no = random.randint(1, len(quotes["data"])) - 1
-        if "quote" in quotes["data"][ran_no]:
-            quote = quotes["data"][ran_no]["quote"]
-            author = quotes["data"][ran_no]["author"]
-            if os.name == "nt" or args.nocolor:
-                quote = "\"" + quote + "\""
-                author = "--" + author
-                white_code = ""
-            else:
-                quote = "\033[1;36m" + "\"" + quote + "\"" + "\033[1;m"
-                author = "\033[1;35m" + "--" + author + "\033[1;m"
-                white_code = "\x1b[0m"
-            output = quote + "\n\t\t" + author
-            print(output + white_code)
+        quote = quotes["data"][random.randrange(len(quotes["data"]))]
+        if (os.name == "nt") or args.nocolor:
+            print("\"{0}\"\n\t\t{1}".format(quote["quote"], quote["author"]))
         else:
-            print ("---------------Debug info begins:--------------")
-            print("This is a message indicating an error in your json database:")
-            print("No key 'quote' is found in the file: "+filename+", item_index = "+str(ran_no))
-            print("Possibly this json file uses capitalized inital letter in its key.")
-            print("You might need to change substitute 'Quote' to 'quote', and 'Author' to 'author'.")
-            print("Try to print this problematic item:\n"+str(quotes["data"][ran_no]))
-            print ("---------------Debug info ends:--------------")
-            cmd_tmp = 'sed -i "s/\"Author\"/\"author\"/g; s/\"Quote\"/\"quote\"/g" '+filename
-            print("Try to fix the problem by using command:")
-            print(cmd_tmp)
-            os.system(cmd_tmp)
-            print("Let's check the output:")
-            f_tmp = open(filename)
-            quotes = json.load(f_tmp)
-            print(str(quotes["data"][ran_no]))
-            print("Hopfully this problem has been solved.")
+            print("\033[1;36m\"{0}\"\n\t\t\033[1;35m--{1}".format(quote["quote"], quote["author"]))
 
 
 if __name__ == "__main__":
